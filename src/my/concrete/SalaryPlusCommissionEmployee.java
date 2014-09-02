d3821622dcfdbb90cc5f7017179cab6ae86eface
viewt_000 <viewt_000@192.168.0.17>
2014-09-02 12:02:11 -0500

    Added Employee subtype to my.concrete: SalaryPlusCommissionEmployee: receives both salary and commission

---
+++ src/my/concrete/SalaryPlusCommissionEmployee.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package my.concrete;
+import inheritancepractice.*;
+/**
+ *
+ * @author viewt_000
+ */
+public class SalaryPlusCommissionEmployee extends SalariedEmployee{
+    private double sales;
+    private double commissionRate;
+    
+    public SalaryPlusCommissionEmployee(Name name, double salary, short daysPaid, double commissionRate)
+    {
+        super(name, salary, daysPaid);
+        this.commissionRate = commissionRate;
+        this.sales = 0;
+    }
+    
+    public void addSales(double amount)
+    {
+        if(amount >= 0)
+        {
+            sales += amount;
+        }
+    }
+    
+    @Override
+    public double getPay()
+    {
+        double commission = sales * commissionRate;
+        sales = 0;
+        return super.getPay() + commission;
+    }
+}
